67254e9ead687dd99cbb41015a8cb2734d32afc0
Millena Almeida <100153774+Millenac@users.noreply.github.com>
2024-08-18 09:05:10 -0300

    Update 04_vogal_conso.py

--- estrutura_desisao/04_vogal_conso.py
+++ estrutura_desisao/04_vogal_conso.py
@@ -1,4 +1,4 @@
-letra = input("Digite uma letra: ")
+letra = input("Digite uma letra: ").lower()
 
 if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
     print(f"A letra {letra} é uma vogal!")
